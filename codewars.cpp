e0bd4abfca38a331c72eb49bc1e18058b2ce886a
Dimendr <dsushko13@yandex.ru>
2023-04-24 17:30:14 +0300

    Init

---
+++ codewars.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+std::vector<std::string> number(const std::vector<std::string> lines) {
+    int r = 49;
+    for (auto i : lines) {
+
+        i.insert(i.begin(), (char)r);
+        i.insert(i.begin() + 1, ':');
+        i.insert(i.begin() +2, ' ');
+        
+        r++;
+    }
+    return lines;
+}
+
+int main() {
+   number({ "a", "b", "c" });
+   for (auto i : number) {
+       cout << i << endl;
+   }
+}
